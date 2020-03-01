cd3d069b9d69e3515e1217ca438ab491084ee9de
Simo Khouili <m.simokhouili@gmail.com>
2020-03-01 23:40:26 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    # before.reject works on the first and the third test. 
+    count_sentences = self.split(/[.?!]/).reject{|string| string.empty?}
+    return count_sentences.count
+
+      # tried to use the if stat, but I could not pass the third
+      # test
+  #   count_sentences = self.split(/[.?!]/)
+  #   count_sentences = self.reject{|string| string.empty?}
 
   end
 end
